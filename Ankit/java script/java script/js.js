de535880ca3202ce0cbb969ff67371e0e3135fc4
ankityadav932 <ankitrao.a1@gmail.com>
2020-01-22 15:56:07 +0530

    java

---
+++ Ankit/java script/java script/js.js
@@ -0,0 +1,10 @@
+function cb() {
+	//var a = document.getElementById("a");
+	//var b = document.getElementById("b");
+	//var c = a.value * b.value ;	
+	//document.getElementById("result").innerHTML ="<u>" + c + "</u>";
+   var a = document.getElementById('a');
+   var b = document.getElementById('b');
+   var c = a.value * b.value ;
+   document.getElementById('result').innerHTML ="<u><strong>" + c + "</strong></u>";
+}
